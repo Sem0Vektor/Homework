1a4b4058a12edb22b6f2ba3012f57a500a8afcb1
Sem0Vektor <mozziella@proton.me>
2023-01-26 19:15:29 +0500

    task27

---
+++ Homework4Functions/task27/Program.cs
@@ -0,0 +1,28 @@
+﻿int SumOfDigits(int number)
+{   
+    if(number < 0)
+    {   
+        number = number*(-1);
+        string str;
+        str = number.ToString();
+        return str.Length;
+    }
+    else
+    {
+        string str;
+        str = number.ToString();
+        return str.Length;
+    }
+}
+
+Console.Write("Enter number ");
+int n = int.Parse(Console.ReadLine()!);
+
+    Console.WriteLine();
+
+Console.Write($"Sum of digits is {SumOfDigits(n)}");
+
+//string s; 
+//int n = 432;
+//s = n.ToString();
+//Console.WriteLine(s.Length);
