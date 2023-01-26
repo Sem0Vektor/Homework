c65229155c915141e39c497709cc46c05cde0d4e
Sem0Vektor <mozziella@proton.me>
2023-01-26 18:37:08 +0500

    task25

---
+++ Homework4Functions/task25/Program.cs
@@ -0,0 +1,16 @@
+﻿int PowerOneNumberWithAnother(int numberToPower, int numberThatPowers)
+{
+    return Convert.ToInt32(Math.Pow(numberToPower, numberThatPowers));
+}
+
+Console.Write("Enter number to power ");
+int firstNumber = int.Parse(Console.ReadLine()!);
+
+    Console.WriteLine();
+
+Console.Write("Enter power number ");
+int secondNumber = int.Parse(Console.ReadLine()!);
+
+    Console.WriteLine();
+
+Console.Write($"The result is {PowerOneNumberWithAnother(firstNumber, secondNumber)}");
