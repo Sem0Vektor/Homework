7f5b8439c6f2251ea4bd1d391182444a80133d4c
Sem0Vektor <mozziella@proton.me>
2023-01-27 10:30:27 +0500

    task29

---
+++ Homework4Functions/task29/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int[] CreateAndFillArray()
+{   
+    int[] array = new int[8];
+    for(int i = 0; i < 8; i++)
+    {
+        array[i] = new Random().Next(1, 9);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < 8; i++)
+    {   
+        if(i == 7)
+        {
+            Console.Write($"{array[i]} ");
+        }
+        else
+        {
+            Console.Write($"{array[i]}, ");
+        }
+    }
+    Console.Write("]");
+}
+
+PrintArray(CreateAndFillArray());
