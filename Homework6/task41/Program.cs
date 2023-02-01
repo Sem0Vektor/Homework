5e0a3d159ec74b15559970fa52e5acd1593bd643
Sem0Vektor <mozziella@proton.me>
2023-02-01 19:19:38 +0500

    task 41 & task 43

---
+++ Homework6/task41/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+
+*/
+int[] CreateArrayV3(int length)
+{
+    int[] array = new int[length];
+    Console.WriteLine("Enter numbers ");
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"number {i + 1} = ");
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+    return array;
+}
+
+void PrintArrayV2(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == array.Length - 1) Console.WriteLine($"{array[i]} ");
+        else Console.Write($"{array[i]}, ");
+    }
+}
+
+void AmountOfNumbersAfterNull(int [] array)
+{
+    int amount = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0) amount++;
+    }
+    Console.Write($"Amount of numbers after null is {amount}");
+}
+
+Console.Write("Enter number of numbers ");
+int length = int.Parse(Console.ReadLine()!);
+
+int[] array = new int[length];
+array = CreateArrayV3(length);
+
+PrintArrayV2(array);
+
+AmountOfNumbersAfterNull(array);
---
+++ Homework6/task43/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+*/
+
+void FindIntersectionPoint(double k1, double b1, double k2, double b2)
+{
+    if (k1 == k2)
+    {
+        if (b1 == b2) Console.Write("These lines located at same coordinates");
+        else Console.Write("These lines are parallel");
+    }
+    else
+    {
+        double k = (k1 - k2) * (-1);
+        double b = (b1 - b2);
+        double x = b / k;
+        double y = (k1 * x + b1);
+        Console.Write($"Intersection point located at coordinates ({x};{y})");
+    }
+}
+
+Console.Write("Enter k1 ");
+double k1 = double.Parse(Console.ReadLine()!);
+
+Console.Write("Enter b1 ");
+double b1 = double.Parse(Console.ReadLine()!);
+
+Console.Write("Enter k2 ");
+double k2 = double.Parse(Console.ReadLine()!);
+
+Console.Write("Enter b2 ");
+double b2 = double.Parse(Console.ReadLine()!);
+
+FindIntersectionPoint(k1, b1, k2, b2);
